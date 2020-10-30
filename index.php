6198ba5e32dbf4129f6736b27798c8d50a896547
63070046 <73632927+63070046@users.noreply.github.com>
2020-10-30 12:49:09 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,23 @@
-<?php
-echo "<h2>what is PHP?</h2>";
-echo "Hello ITF<br>";
-?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Hello World ITF</title>
+    </head>
+    <body>
+        <h1>ณภัทร พ่อบุตรดี อาร์ท</h1>
+        <h2>Are you bored?</h2>
+        <p>Two dogs tilting their heads wants to know how you feel.</p>
+        <img src='dogg.gif'>
+        <h3>Meanwhile</h3>
+        <img src='cat.gif'>
+        <p>The cat, jealous their cuteness, is finding out why tilting heads make them look cuter.</p>
+        <p>He found this site: </p>
+        <a href="https://vcahospitals.com/know-your-pet/why-dogs-tilt-their-heads">The site</a>
+        <p>Whatever it says, the cat is gonna do it anyway.</p>
+        <img src="cattilt.gif">
+        <p>Uhhh... this does not like what he thinks.</p>
+        <br>
+        
+    </body>
+
+</html>
