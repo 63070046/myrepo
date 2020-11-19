209a8695b73d3959f7926bca206f7f0419f084c5
artist0123 <napatart1111@gmail.com>
2020-11-19 17:58:09 +0700

    Update remove.php

--- remove.php
+++ remove.php
@@ -7,20 +7,14 @@ if (mysqli_connect_errno($conn))
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 
-
-$name = $_POST['name'];
-$comment = $_POST['comment'];
-$link = $_POST['link'];
-
-
-$sql = "DELETE FROM guestbook WHERE Name = '$name', Comment = '$comment', Link = '$link'";
+$sql = 'DELETE FROM guestbook WHERE ID = '.$_GET['ID'].'';
 
 
 if (mysqli_query($conn, $sql)) {
-    echo "New record created successfully";
+    echo "Data removed";
   } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
   }
-  
+  header( "refresh:5;url=show.php" );
 mysqli_close($conn);
 ?>
