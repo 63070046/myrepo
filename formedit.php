13c4d29d25c68a831aab843eae707b5e66ab9c55
artist0123 <napatart1111@gmail.com>
2020-11-19 19:24:38 +0700

    Update formedit.php

--- formedit.php
+++ formedit.php
@@ -5,12 +5,16 @@
   <?php
   $conn = mysqli_init();
   mysqli_real_connect($conn, 'itfnapat63070046.mysql.database.azure.com', 'itf63070046@itfnapat63070046', 'napatart1111@gmail.com', 'itfLab', 3306);
-  if (mysqli_connect_errno($conn)) {
-    die('Failed to connect to MySQL: ' . mysqli_connect_error());
-  }
+  $id = $_GET['ID'];
 
-  $sql = 'SELECT FROM guestbook WHERE ID = ' . $_GET['ID'] . '';
-  ?>
+	$sql = 'SELECT * FROM guestbook WHERE ID = '.$id.'';
+	$query = mysqli_query($conn, $sql);
+	if(!$query) {
+		header('Location: show.php');
+	}
+	else {
+		$data = mysqli_fetch_assoc($query);
+	}?>
   <title>Comment Form</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -28,16 +32,17 @@
     <div class="container">
       <form action="update.php" method="POST">
         <div class="form-group">
+          <input type="text" name="id" value="<?php echo $data['ID']; ?>" class="form-control d-none" required>
           <label for="name">Name:</label>
-          <input type="text" class="form-control" id="idName" placeholder="Enter name" name="name">
+          <input type="text" class="form-control" id="idName" placeholder="Enter name" name="name" value="<?php echo $data['Name']; ?>">
         </div>
         <div class="form-group">
           <label for="comment">Comment:</label>
-          <input type="text" class="form-control" id="idComment" placeholder="Enter comment" name="comment">
+          <input type="text" class="form-control" id="idComment" placeholder="Enter comment" name="comment" value="<?php echo $data['Comment']; ?>">
         </div>
         <div class="form-group">
           <label for="url">Link:</label>
-          <input type="text" class="form-control" id="idLink" placeholder="Enter Link" name="link">
+          <input type="text" class="form-control" id="idLink" placeholder="Enter Link" name="link" value="<?php echo $data['Link']; ?>">
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
       </form>
