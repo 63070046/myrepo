6e4e4106c1191b7c57423395529f6099097a6f08
artist0123 <napatart1111@gmail.com>
2020-11-19 18:17:37 +0700

    Update remove.php

--- remove.php
+++ remove.php
@@ -1,7 +1,4 @@
 <?php
-header("Refresh:5; url=index.php");
-exit(0);
-
 $conn = mysqli_init();
 mysqli_real_connect($conn, 'itfnapat63070046.mysql.database.azure.com', 'itf63070046@itfnapat63070046', 'napatart1111@gmail.com', 'itfLab', 3306);
 if (mysqli_connect_errno($conn))
@@ -18,4 +15,6 @@ if (mysqli_query($conn, $sql)) {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
   }
 mysqli_close($conn);
+header("Refresh:2; url=show.php");
+exit(0);
 ?>
