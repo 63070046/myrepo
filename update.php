e706bc516f0c359993ea6a9e1dcf6173bdf2ea04
artist0123 <napatart1111@gmail.com>
2020-11-19 15:42:03 +0700

    Remove old form.html and add formadd, formedit

---
+++ update.php
@@ -0,0 +1,26 @@
+<?php
+
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'itfnapat63070046.mysql.database.azure.com', 'itf63070046@itfnapat63070046', 'napatart1111@gmail.com', 'itfLab', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+
+
+$name = $_POST['name'];
+$comment = $_POST['comment'];
+$link = $_POST['link'];
+
+
+$sql = "UPDATE guestbook SET Name = '$name', Comment = '$comment', Link = '$link' WHERE contact_id = '".$contact_id."'";
+
+
+if (mysqli_query($conn, $sql)) {
+    echo "Record updated successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
+  
+mysqli_close($conn);
+?>
